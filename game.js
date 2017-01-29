1a26142bb101e41f81053f137ad06759bd8a12d3
kevinchau321 <kevinchau321@berkeley.edu>
2017-01-28 17:27:01 -0800

    reduce walking speed

--- game.js
+++ game.js
@@ -109,13 +109,13 @@ function update() {
 
 	player.body.velocity.x = 0;
 	if (cursors.left.isDown){
-		player.body.velocity.x = -150;
+		player.body.velocity.x = -75;
 		player.direction = 'left';
 		player.animations.play('left');
 	}
 	else if (cursors.right.isDown)
 	{
-		player.body.velocity.x = 150;
+		player.body.velocity.x = 75;
 		player.direction = 'right';
 		player.animations.play('right');
 	}
