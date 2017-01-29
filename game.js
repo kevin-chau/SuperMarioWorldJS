24f245867f8787226b3f66f306f284d7f446310d
kevinchau321 <kevinchau321@berkeley.edu>
2017-01-28 20:14:26 -0800

    coin sprites

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ function preload() {
 	game.load.image('ground', 'assets/tutorial/platform.png');
 	game.load.image('star', 'assets/tutorial/star.png');
 	game.load.atlasJSONArray('mario', 'assets/sprites/spritesheets/mario.png', 'assets/sprites/spritesheets/mario.json');
+	game.load.atlasJSONArray('items', 'assets/items/items.png', 'assets/items/items.json');
 
 	// game.load.atlasJSONArray('mario', 'assets/sprites/atlases/mario/mario.png', 'assets/sprites/atlases/mario/mario.json');
 
@@ -80,18 +81,19 @@ function create() {
 
 	/*
 	********************************************
-	STARS
+	Coins
 	********************************************
 	*/
 
-	stars = game.add.group();
+	coins = game.add.group();
 
-	stars.enableBody = true;
+	coins.enableBody = true;
 
 	for (var i = 0; i < 12; i++){
-		var star = stars.create(i*70,0,'star');
-		star.body.gravity.y = 6;
-		star.body.bounce.y = 0.7 + Math.random() * 0.2;
+		var coin = coins.create(i*70,0,'items');
+		coin.frame = 0;
+		coin.body.gravity.y = 6;
+		coin.body.bounce.y = 0.7 + Math.random() * 0.2;
 	}
 
 	/*
@@ -153,13 +155,13 @@ function update() {
     // }
 	}
 
-	game.physics.arcade.collide(stars, platforms);
-	game.physics.arcade.overlap(player, stars, collectStar, null, this);
+	game.physics.arcade.collide(coins, platforms);
+	game.physics.arcade.overlap(player, coins, collectCoin, null, this);
 
 }
 
-function collectStar(player, star) {
-	star.kill();
+function collectCoin(player, coin) {
+	coin.kill();
 
 	score += 10;
 	scoreText.text = 'Score: ' + score;
