c54cc40defb7112907276e4b6066faa1f90c8d0b
kevinchau321 <kevinchau321@berkeley.edu>
2017-02-01 16:47:08 -0800

    removed coins, using https for favicon

--- src/game.js
+++ src/game.js
@@ -112,15 +112,15 @@ function create() {
 
 	coins.enableBody = true;
 
-	for (var i = 0; i < 12; i++){
-		var coin = coins.create(i*30, game.world.height - 40,'items');
-		coin.anchor.setTo(.5,.5);
-		coin.body.height = 15;
-		coin.body.width = 12;
-		coin.frame = 4;
-		coin.animations.add('spin', [4,5,6,7], 8, true);
-		coin.animations.play('spin');
-	}
+	// for (var i = 0; i < 12; i++){
+	// 	var coin = coins.create(i*30, game.world.height - 40,'items');
+	// 	coin.anchor.setTo(.5,.5);
+	// 	coin.body.height = 15;
+	// 	coin.body.width = 12;
+	// 	coin.frame = 4;
+	// 	coin.animations.add('spin', [4,5,6,7], 8, true);
+	// 	coin.animations.play('spin');
+	// }
 
 
 	/*
