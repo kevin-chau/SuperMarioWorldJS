d94a32478762b00da00a21c558cdb95533a73842
kevinchau321 <kevinchau321@berkeley.edu>
2017-02-13 01:38:17 -0800

    added build to gh-pages

--- src/js/states/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Boot} from './boot';
-export {default as Preload} from './preload';
-export {default as Menu} from './menu';
-export {default as Play} from './play';
-export {default as Over} from './over';
